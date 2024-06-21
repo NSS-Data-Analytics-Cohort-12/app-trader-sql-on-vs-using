4263bc70388dd508491ce6db3797741d101c9b32
Brandon Coughlin <brandoncoughlin@macbook-air.lan>
2024-06-20 21:10:29 -0500

    brandon_thursday_work

--- scripts/app_trader_brandon.sql
+++ scripts/app_trader_brandon.sql
@@ -49,8 +49,107 @@
 -- select distinct(genres)
 -- from play_store_apps
 
---Google Play apps. We decided that genre wasn't good to narrow down apps with. 
+--Google Play apps. We decided that genre wasn't good to narrow down apps with.
+
+-- Select name,
+-- 	ROUND(AVG((p.rating + a.rating)/2),2) AS avg_rating,
+-- 	SUM(CAST(a.review_count AS DECIMAL) + p.review_count) AS total_reviews,
+-- 	CAST(p.price AS DECIMAL) as google_price
+-- from play_store_apps as p
+-- inner Join app_store_apps as a
+-- USING(name)
+-- 	GROUP BY name, google_price
+-- 	HAVING SUM(CAST(a.review_count AS INTEGER) + p.review_count) >= 1000
+-- 	ORDER BY total_reviews DESC
+
+-- --TO GET THE AVG PRICE FOR BOTH APPS
+-- SELECT name, 
+-- 	CASE 
+-- 	WHEN p.price ::  >= a.price  THEN p.price
+-- 	WHEN a.price >= p.price :: NUMERIC THEN a.price
+-- 	ELSE 'N/A' END AS app_price
+-- 	--AVG((p.price :: INTEGER) + a.price)  AS avg_price
+-- FROM app_store_apps AS a
+-- INNER JOIN play_store_apps AS p
+-- USING(name);
+-- --GROUP BY name
+-- --ORDER BY avg_price DESC;
 
 
+-- select name, a.price::money, p.price::money
+-- from play_store_apps as p
+-- Join app_store_apps as a
+-- using (name)
+
+--This gave us the prices for each app converted to the proper data types to compare. 
+
+-- Select name,
+-- 	ROUND(AVG((p.rating + a.rating)/2),2) AS avg_rating,
+-- 	SUM(CAST(a.review_count AS INTEGER) + p.review_count) AS total_reviews,
+-- 	p.install_count,
+	
+-- from play_store_apps as p
+-- inner Join app_store_apps as a
+-- USING(name)
+-- 	GROUP BY name, p.install_count
+-- 	HAVING SUM(CAST(a.review_count AS INTEGER) + p.review_count) >= 1000
+-- 	ORDER BY total_reviews DESC
+
+-- select name, price
+-- from app_store_apps
+-- 	where name = 'Doodle Jump'
+
+-- Select name, p.install_count::NUMERIC, a.primary_genre,
+-- 	ROUND(AVG((p.rating + a.rating)/2),2) AS avg_rating,
+-- 	SUM(CAST(a.review_count AS INTEGER) + p.review_count) AS total_reviews,
+-- 	a.price::money AS app_store_price, 
+-- 	p.price::money AS play_store_price
+-- from play_store_apps as p
+-- inner Join app_store_apps as a
+-- USING(name)
+-- 	GROUP BY name, a.price::money, p.price::money,p.install_count, a.primary_genre
+-- 	HAVING SUM(CAST(a.review_count AS INTEGER) + p.review_count) >= 1000
+-- 	ORDER BY p.install_count::NUMERIC DESC
+
+-- select trim('+' from "install_count")
+-- from play_store_apps
+
+-- Select
+-- 	name,
+-- 	REPLACE(REPLACE(install_count, '+',''), ',', '') :: NUMERIC as pi_count,
+-- 	p.content_rating,
+-- 	a.content_rating,
+-- 	ROUND(AVG((p.rating + a.rating)/2),2) AS avg_rating,
+-- 	SUM(CAST(a.review_count AS INTEGER) + p.review_count) AS total_reviews,
+-- 	a.price::money AS app_store_price,
+-- 	p.price::money AS play_store_price
+-- from play_store_apps as p
+-- inner Join app_store_apps as a
+-- USING(name)
+-- 	GROUP BY name, a.price::money, p.price::money, p.install_count, p.content_rating, a.content_rating
+-- 	HAVING SUM(CAST(a.review_count AS INTEGER) + p.review_count) >= 1000
+-- 	ORDER BY pi_count DESC;
+
+Select
+	name,
+	CASE 
+	WHEN TRIM(REPLACE( p.price ,'$', ''))::NUMERIC >= a.price THEN TRIM(REPLACE( p.price ,'$', ''))::NUMERIC
+	WHEN a.price >= TRIM(REPLACE (p.price ,'$', ''))::NUMERIC THEN a.price
+	END AS app_price,
+	REPLACE(REPLACE(install_count, '+',''), ',', '') :: NUMERIC as pi_count,
+	p.content_rating,
+	a.content_rating,
+	ROUND(AVG((p.rating + a.rating)/2),2) AS avg_rating,
+	SUM(CAST(a.review_count AS INTEGER) + p.review_count) AS total_reviews,
+	((ROUND(AVG((p.rating + a.rating)/2),2) * 2)+1) AS expected_life
+from play_store_apps as p
+inner Join app_store_apps as a
+USING(name)
+	GROUP BY name, p.install_count, p.content_rating, a.content_rating, p.price, a.price
+	HAVING 
+		SUM(CAST(a.review_count AS INTEGER) + p.review_count) >= 1000 AND
+		ROUND(AVG((p.rating + a.rating)/2),2) >= 4 AND 
+		REPLACE(REPLACE(install_count, '+',''), ',', '') :: NUMERIC >= 500000000
+	ORDER BY pi_count DESC;
 
 
